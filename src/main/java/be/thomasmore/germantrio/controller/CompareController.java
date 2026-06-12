9244ec3aadfc27ce97b9df1565e466f6b86ac605
gabriele pjetra <gabriele.pjetra@student.thomasmore.be>
2026-06-12 21:34:44 +0200

    created CompareController for the comparator tool

--- src/main/java/be/thomasmore/germantrio/controller/CompareController.java
+++ src/main/java/be/thomasmore/germantrio/controller/CompareController.java
@@ -1,14 +1,13 @@
 package be.thomasmore.germantrio.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class CompareController {
 
-    @GetMapping({"/","/index"})
-    public String index() {
-        return "index";
+    @GetMapping("compare")
+    public String compare(){
+        return "compare";
     }
 }

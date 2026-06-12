2ceb3c62553f63a1d6008f1d9571f0d06cb1ef74
gabriele pjetra <gabriele.pjetra@student.thomasmore.be>
2026-06-12 22:22:08 +0200

    added brandrepository to home controller to display brand cards in the homepage

--- src/main/java/be/thomasmore/germantrio/controller/HomeController.java
+++ src/main/java/be/thomasmore/germantrio/controller/HomeController.java
@@ -1,13 +1,22 @@
 package be.thomasmore.germantrio.controller;
 
+import be.thomasmore.germantrio.repository.BrandRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    private final BrandRepository brandRepository;
+
+    public HomeController(BrandRepository brandRepository) {
+        this.brandRepository = brandRepository;
+    }
+
     @GetMapping({"/","/index"})
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("brands", brandRepository.findAll());
         return "index";
     }
 

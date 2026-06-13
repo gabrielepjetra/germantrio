8ec7c3156cc70a3a56b9f90580d6c0e99ba5c658
gabriele pjetra <gabriele.pjetra@student.thomasmore.be>
2026-06-13 20:40:42 +0200

    added first logic to compare controller

--- src/main/java/be/thomasmore/germantrio/controller/CompareController.java
+++ src/main/java/be/thomasmore/germantrio/controller/CompareController.java
@@ -1,13 +1,31 @@
 package be.thomasmore.germantrio.controller;
 
+import be.thomasmore.germantrio.repository.CarModelRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CompareController {
 
+    private final CarModelRepository carModelRepository;
+
+    public CompareController(CarModelRepository carModelRepository) {
+        this.carModelRepository = carModelRepository;
+    }
+
     @GetMapping("/compare")
-    public String compare(){
+    public String compare(@RequestParam(required = false) Long leftCarId, Model model) {
+        if (leftCarId != null) {
+            return carModelRepository.findById(leftCarId)
+                    .map(car -> {
+                        model.addAttribute("leftCar", car);
+                        return "compare";
+                    })
+                    .orElse("redirect:/compare");
+        }
+
         return "compare";
     }
 }
